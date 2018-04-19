ed0fad59092e43655a9c3f79d84b570a2ecb45cd
John Topley <john.topley@ons.gov.uk>
2018-04-19 19:19:59 +0100

    Fix Codacy issues - remove unused imports

--- src/main/java/uk/gov/ons/fwmt/gateway/service/impl/IngesterServiceImpl.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/service/impl/IngesterServiceImpl.java
@@ -1,30 +1,17 @@
 package uk.gov.ons.fwmt.gateway.service.impl;
 
-import com.consiliumtechnologies.schemas.mobile._2015._05.optimisemessages.CreateJobRequest;
-import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.SendCreateJobRequestMessage;
-import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.SendCreateJobRequestMessageResponse;
-import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.SendMessageRequestInfo;
+import java.util.Iterator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import uk.gov.ons.fwmt.gateway.entity.LegacyJobEntity;
 import uk.gov.ons.fwmt.gateway.entity.LegacyLeaverEntity;
 import uk.gov.ons.fwmt.gateway.entity.LegacySampleEntity;
 import uk.gov.ons.fwmt.gateway.entity.LegacyStaffEntity;
-import uk.gov.ons.fwmt.gateway.entity.LegacyUserEntity;
-import uk.gov.ons.fwmt.gateway.repo.monitoring.LegacyJobsRepo;
-import uk.gov.ons.fwmt.gateway.repo.monitoring.LegacyUsersRepo;
 import uk.gov.ons.fwmt.gateway.repo.reception.LegacyLeaversRepo;
 import uk.gov.ons.fwmt.gateway.repo.reception.LegacySampleRepo;
 import uk.gov.ons.fwmt.gateway.repo.reception.LegacyStaffRepo;
 import uk.gov.ons.fwmt.gateway.service.IngesterService;
-import uk.gov.ons.fwmt.gateway.utility.TMMessageSubmitter;
-import uk.gov.ons.fwmt.gateway.utility.csvconverter.LegacyCreateJobRequestFactory;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class IngesterServiceImpl implements IngesterService {
