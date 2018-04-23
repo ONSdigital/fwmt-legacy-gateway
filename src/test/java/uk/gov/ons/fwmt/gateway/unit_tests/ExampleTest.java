d44bfbdb0b3833cdf2f1de4db35f2c3217780c2e
ThomasPootQA <thomas.poot@qa.com>
2018-04-23 14:30:50 +0100

    Filled out the example test class

--- src/test/java/uk/gov/ons/fwmt/gateway/unit_tests/ExampleTest.java
+++ src/test/java/uk/gov/ons/fwmt/gateway/unit_tests/ExampleTest.java
@@ -1,10 +1,12 @@
 package uk.gov.ons.fwmt.gateway.unit_tests;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ExampleTest {
     @Test
     public void test() {
-
+        // An empty test to prove that tests are run
+        Assert.assertTrue(true);
     }
 }
