db79eb0cad444b26bc1fda0afce308cd77a2d56e
ThomasPootQA <35296806+ThomasPootQA@users.noreply.github.com>
2018-04-23 15:07:48 +0100

    Revert "Syncing with datamap"

--- src/main/java/uk/gov/ons/fwmt/gateway/repo/monitoring/LegacyUsersRepo.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/repo/monitoring/LegacyUsersRepo.java
@@ -6,7 +6,7 @@ import uk.gov.ons.fwmt.gateway.entity.LegacyUserEntity;
 
 public interface LegacyUsersRepo extends CrudRepository<LegacyUserEntity, Long> {
     Iterable<LegacyUserEntity> findAll();
-    LegacyUserEntity findByAuthno(String authNo);
+    LegacyUserEntity findByAuthNo(String authNo);
     boolean existsByAuthNo(String authNo);
     LegacyUserEntity findByTmusername(String tmUsername);
     boolean existsByTmusername(String proposedTMUsername);
--- src/main/java/uk/gov/ons/fwmt/gateway/utility/readers/LegacyStaffReader.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/utility/readers/LegacyStaffReader.java
@@ -14,56 +14,28 @@ import uk.gov.ons.fwmt.gateway.entity.LegacyStaffEntity;
 @Slf4j
 public class LegacyStaffReader extends CsvToBean<LegacyStaffEntity> {
 
-    private static final String EMPLOYEENO = "employeeNo";
-    private static final String INTNUM = "authNo";
-    private static final String TITLE = "title";
-    private static final String FIRSTNAME = "firstname";
-    private static final String SURNAME = "surname";
-    private static final String SEX = "sex";
-    private static final String ADDRESS1 = "address1";
-    private static final String ADDRESS2 = "address2";
-    private static final String ADDRESS3 = "address3";
-    private static final String ADDRESS4 = "address4";
-    private static final String POSTCODE = "postcode";
-    private static final String TEL = "tel";
-    private static final String TEL2 = "tel2";
-    private static final String MOBILE_TEL = "mobileTel";
-    private static final String EMERGENCY_TEL = "emergencyTel";
-    private static final String EMAIL = "email";
-    private static final String GOR = "gor";
-    private static final String COUNTY = "county";
-    private static final String GRID_REF = "gridRef";
-    private static final String MAINFIELDFORCE = "mainFieldForce";
-    private static final String DATEOFBIRTH = "dateOfBirth";
-    private static final String INACTIVENO = "inactiveNo";
-    private static final String EASTING = "easting";
-    private static final String NORTHING = "northing";
-    private static final String FIELDMANAGER = "fieldManager";
-    private static final String MANAGER_EMPLOYEENO = "managerEmployeeNo";
-    private static final String MANAGER_AUTH = "managerAuth";
-    private static final String MANAGER_FIRSTNAME = "managerFirstname";
-    private static final String MANAGER_SURNAME = "managerSurname";
-    private static final String REGION = "region";
-    private static final String REGIONNAME = "regionName";
-    private static final String RM_EMPLOYEENO = "rmEmployeeNo";
-    private static final String RM_FIRSTNAME = "rmFirstname";
-    private static final String RM_SURNAME = "rmSurname";
-    private static final String[] NEW_USER_COLUMNS = new String[] { EMPLOYEENO, INTNUM, TITLE, FIRSTNAME, SURNAME, SEX,
-            ADDRESS1, ADDRESS2, ADDRESS3, ADDRESS4, POSTCODE, TEL, TEL2, MOBILE_TEL, EMERGENCY_TEL, EMAIL, GOR, COUNTY,
-            GRID_REF, MAINFIELDFORCE, DATEOFBIRTH, INACTIVENO, EASTING, NORTHING, FIELDMANAGER, MANAGER_EMPLOYEENO,
-            MANAGER_AUTH, MANAGER_FIRSTNAME, MANAGER_SURNAME, REGION, REGIONNAME, RM_EMPLOYEENO, RM_FIRSTNAME, RM_SURNAME };
+	private static final String EMPLOYEENO = "employeeNo";
+	private static final String AUTHNO = "authNo";
+	private static final String FORENAME = "forename";
+	private static final String SURNAME = "surname";
+	private static final String JOB_TITLE = "jobTitle";
+	private static final String EMAIL = "email";
+	private static final String PHONE = "phone";
+	private static final String USER_TYPE = "userType";
+	private static final String[] NEW_USER_COLUMNS = new String[] { EMPLOYEENO, AUTHNO, FORENAME, SURNAME, JOB_TITLE,
+			EMAIL, PHONE, USER_TYPE };
 
-    private CSVReader csvReader;
-    private Iterator<String[]> csvIterator;
-    private ColumnPositionMappingStrategy<LegacyStaffEntity> columnPositionMappingStrategy;
-
-    public LegacyStaffReader(InputStream stream) {
-        columnPositionMappingStrategy = new ColumnPositionMappingStrategy<>();
-        columnPositionMappingStrategy.setType(LegacyStaffEntity.class);
+	private CSVReader csvReader;
+	private Iterator<String[]> csvIterator;
+	private ColumnPositionMappingStrategy<LegacyStaffEntity> columnPositionMappingStrategy;
+	
+	public LegacyStaffReader(InputStream stream) {
+		columnPositionMappingStrategy = new ColumnPositionMappingStrategy<>();
+		columnPositionMappingStrategy.setType(LegacyStaffEntity.class);
         columnPositionMappingStrategy.setColumnMapping(NEW_USER_COLUMNS);
         csvReader = new CSVReader(new InputStreamReader(stream));
-        csvIterator = csvReader.iterator();
-        this.setCsvReader(csvReader);
-        this.setMappingStrategy(columnPositionMappingStrategy);
-    }
+		csvIterator = csvReader.iterator();
+		this.setCsvReader(csvReader);
+		this.setMappingStrategy(columnPositionMappingStrategy);
+	}
 }
