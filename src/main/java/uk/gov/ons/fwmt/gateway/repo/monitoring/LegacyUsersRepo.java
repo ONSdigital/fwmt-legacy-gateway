bbcc5ec663581a993ef17a43c569d85323df5bbf
ThomasPootQA <thomas.poot@qa.com>
2018-04-24 10:36:33 +0100

    Minor name-correction in LegacyUsersRepo

--- src/main/java/uk/gov/ons/fwmt/gateway/repo/monitoring/LegacyUsersRepo.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/repo/monitoring/LegacyUsersRepo.java
@@ -6,8 +6,8 @@ import uk.gov.ons.fwmt.gateway.entity.LegacyUserEntity;
 
 public interface LegacyUsersRepo extends CrudRepository<LegacyUserEntity, Long> {
     Iterable<LegacyUserEntity> findAll();
-    LegacyUserEntity findByAuthno(String authNo);
-    boolean existsByAuthno(String authNo);
+    LegacyUserEntity findByAuthNo(String authNo);
+    boolean existsByAuthNo(String authNo);
     LegacyUserEntity findByTmusername(String tmUsername);
     boolean existsByTmusername(String proposedTMUsername);
 }
