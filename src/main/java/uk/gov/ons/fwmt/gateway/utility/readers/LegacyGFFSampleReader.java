8bc3f2dbb6d6fa9563aede2be039e83c5d886af1
ThomasPootQA <thomas.poot@qa.com>
2018-04-25 16:25:32 +0100

    Upgraded the Readers with the correct logic to parse from the current data map Added tests for the above, which verify that the CSV parsers work to *our own* view of them

--- src/main/java/uk/gov/ons/fwmt/gateway/utility/readers/LegacyGFFSampleReader.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/utility/readers/LegacyGFFSampleReader.java
@@ -3,65 +3,80 @@ package uk.gov.ons.fwmt.gateway.utility.readers;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.ons.fwmt.gateway.entity.LegacySampleEntity;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
+import java.util.function.BiConsumer;
 
 public class LegacyGFFSampleReader {
-  private static final Map<String, String> SAMPLE_GFF_DATA_COLUMN_MAP;
+  static final List<String> CSV_HEADERS;
+
+  static final List<String> DATA_FIELDS;
+
+  static final Map<String, String> SAMPLE_GFF_DATA_COLUMN_MAP;
 
   static {
+    List<String> csvHeaders = new ArrayList<>();
+    List<String> dataFields = new ArrayList<>();
     Map<String, String> map = new HashMap<>();
-    map.put("Serno", "serno");
-    map.put("TLA", "tla");
-    map.put("Year", "year");
-    map.put("Month", "month");
-    map.put("Stage", "stage");
-    map.put("Wave", "wave");
-    map.put("AddressLine1", "addressLine1");
-    map.put("AddressLine2", "addressLine2");
-    map.put("AddressLine3", "addressLine3");
-    map.put("addressLine4", "addressLine4");
-    map.put("District", "district");
-    map.put("PostTown", "postTown");
-    map.put("Postcode", "postcode");
-    map.put("Quota", "quota");
-    map.put("AddressNo", "addressNo");
-    map.put("OSGridRef", "osGridRef");
-    map.put("Issue_No", "issueNo");
-    map.put("Part", "part");
-    map.put("Auth", "auth");
-    map.put("EmployeeNo", "employeeNo");
-    map.put("Last_Updated", "lastUpdated");
-    map.put("Rand", "rand");
-    map.put("LAUA", "laua");
-    map.put("LAUA_Name", "lauaName");
-    map.put("Ward", "ward");
-    map.put("Ward_Name", "wardName");
-    map.put("MO", "mo");
-    map.put("DiffAddInd", "diffAddInd");
-    map.put("GFFMU", "gffmu");
-    map.put("OldSerial", "oldSerial");
-    map.put("SubSample", "subSample");
-    map.put("ADULT2", "adult2");
-    map.put("ADULT3", "adult3");
-    map.put("ADULT4", "adult4");
-    map.put("ADULT5", "adult5");
-    map.put("ADULT6", "adult6");
-    map.put("ADULT7", "adult7");
-    map.put("ADULT8", "adult8");
-    map.put("ADULT9", "adult9");
-    map.put("ADULT10", "adult10");
-    map.put("ADULT11", "adult11");
-    map.put("ADULT12", "adult12");
-    map.put("ADULT13", "adult13");
-    map.put("ADULT14", "adult14");
+    BiConsumer<String, String> register = (csvHeader, dataField) -> {
+      csvHeaders.add(csvHeader);
+      dataFields.add(dataField);
+      map.put(csvHeader, dataField);
+    };
+    register.accept("Serno", "serno");
+    register.accept("TLA", "tla");
+    register.accept("Stage", "stage");
+    register.accept("Wave", "wave");
+    register.accept("Prem1", "prem1");
+    register.accept("Prem2", "prem2");
+    register.accept("Prem3", "prem3");
+    register.accept("Prem4", "prem4");
+    register.accept("District", "district");
+    register.accept("PostTown", "postTown");
+    register.accept("Postcode", "postcode");
+    register.accept("Quota", "quota");
+    register.accept("AddressNo", "addressNo");
+    register.accept("OSGridRef", "osGridRef");
+    register.accept("Year", "year");
+    register.accept("Month", "month");
+    register.accept("LAUA", "laua");
+    register.accept("LAUA_Name", "lauaName");
+    register.accept("SubSample", "subSample");
+    register.accept("Rand", "rand");
+    register.accept("?", "kishGrid"); // TODO figure out what the name in the CSV is
+    register.accept("ADULT2", "adult2");
+    register.accept("ADULT3", "adult3");
+    register.accept("ADULT4", "adult4");
+    register.accept("ADULT5", "adult5");
+    register.accept("ADULT6", "adult6");
+    register.accept("ADULT7", "adult7");
+    register.accept("ADULT8", "adult8");
+    register.accept("ADULT9", "adult9");
+    register.accept("ADULT10", "adult10");
+    register.accept("ADULT11", "adult11");
+    register.accept("ADULT12", "adult12");
+    register.accept("ADULT13", "adult13");
+    register.accept("ADULT14", "adult14");
+    register.accept("Telno", "telno");
+    register.accept("Issue_No", "issueNo");
+    register.accept("Part", "part");
+    register.accept("Auth", "auth");
+    register.accept("EmployeeNo", "employeeNo");
+    register.accept("Last_Updated", "lastUpdated");
+    register.accept("Ward", "ward");
+    register.accept("Ward_Name", "wardName");
+    register.accept("MO", "mo");
+    register.accept("DiffAddInd", "diffAddInd");
+    register.accept("GFFMU", "gffmu");
+    register.accept("OldSerial", "oldSerial");
+    CSV_HEADERS = csvHeaders;
+    DATA_FIELDS = dataFields;
     SAMPLE_GFF_DATA_COLUMN_MAP = map;
   }
 
@@ -78,12 +93,64 @@ public class LegacyGFFSampleReader {
         .build();
   }
 
-  public Iterator<LegacySampleEntity> iterator() {
+  public LegacyGFFSampleIterator iterator() {
     return new LegacyGFFSampleIterator(csvToBean.iterator());
   }
 
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  static public class LegacyGFFSampleEntityRaw {
+    String serno;
+    String tla;
+    String stage;
+    String wave;
+    String prem1;
+    String prem2;
+    String prem3;
+    String prem4;
+    String district;
+    String postTown;
+    String postcode;
+    String quota;
+    String addressNo;
+    String osGridRef;
+    String year;
+    String month;
+    String laua;
+    String lauaName;
+    String subSample;
+    String rand;
+    String kishGrid;
+    String adult2;
+    String adult3;
+    String adult4;
+    String adult5;
+    String adult6;
+    String adult7;
+    String adult8;
+    String adult9;
+    String adult10;
+    String adult11;
+    String adult12;
+    String adult13;
+    String adult14;
+    String telno;
+    String issueNo;
+    String part;
+    String auth;
+    String employeeNo;
+    String lastUpdated;
+    String ward;
+    String wardName;
+    String mo;
+    String diffAddInd;
+    String gffmu;
+    String oldSerial;
+  }
+
   /// This iterator strips excess data out of the LegacyGFFSampleEntityRaw structure as it iterates
-  private class LegacyGFFSampleIterator implements Iterator<LegacySampleEntity> {
+  class LegacyGFFSampleIterator implements Iterator<LegacySampleEntity> {
     Iterator<LegacyGFFSampleEntityRaw> rawIterator;
 
     LegacyGFFSampleIterator(Iterator<LegacyGFFSampleEntityRaw> rawIter) {
@@ -94,63 +161,33 @@ public class LegacyGFFSampleReader {
       return rawIterator.hasNext();
     }
 
+    LegacyGFFSampleEntityRaw nextRaw() {
+      return rawIterator.next();
+    }
+
     @Override public LegacySampleEntity next() {
       LegacyGFFSampleEntityRaw raw = rawIterator.next();
       if (raw == null) {
         return null;
       }
       LegacySampleEntity entity = new LegacySampleEntity();
-      // TODO add fields
+      entity.setSerno(raw.getSerno());
+      entity.setTla(raw.getTla());
+      entity.setStage(raw.getStage());
+      entity.setQuota(raw.getQuota());
+      entity.setAuthNo(raw.getAuth());
+      entity.setEmployeeNo(raw.getEmployeeNo());
+      entity.setAddressLine1(raw.getPrem1());
+      entity.setAddressLine2(raw.getPrem2());
+      entity.setAddressLine3(raw.getPrem3());
+      entity.setAddressLine4(raw.getPrem4());
+      entity.setDistrict(raw.getDistrict());
+      entity.setPostTown(raw.getPostTown());
+      entity.setPostcode(raw.getPostcode());
+      entity.setAddressNo(raw.getAddressNo());
+      entity.setOsGridRef(raw.getOsGridRef());
+      entity.setKishGrid(null);
       return entity;
     }
-  }
-
-  @Data
-  @NoArgsConstructor
-  private class LegacyGFFSampleEntityRaw {
-    private String serno;
-    private String tla;
-    private String year;
-    private String month;
-    private String stage;
-    private String wave;
-    private String addressLine1;
-    private String addressLine2;
-    private String addressLine3;
-    private String addressLine4;
-    private String district;
-    private String postTown;
-    private String postcode;
-    private String quota;
-    private String addressNo;
-    private String osGridRef;
-    private String issueNo;
-    private String part;
-    private String auth;
-    private String employeeNo;
-    private String lastUpdated;
-    private String rand;
-    private String laua;
-    private String lauaName;
-    private String ward;
-    private String wardName;
-    private String mo;
-    private String diffAddInd;
-    private String gffmu;
-    private String oldSerial;
-    private String subSample;
-    private String adult2;
-    private String adult3;
-    private String adult4;
-    private String adult5;
-    private String adult6;
-    private String adult7;
-    private String adult8;
-    private String adult9;
-    private String adult10;
-    private String adult11;
-    private String adult12;
-    private String adult13;
-    private String adult14;
   }
 }
