5feb7c8632ef8566f078abb599442b3c4ddd1569
ThomasPootQA <thomas.poot@qa.com>
2018-04-26 10:57:17 +0100

    Reformatted LegacyEndpointRESTController

--- src/main/java/uk/gov/ons/fwmt/gateway/controller/LegacyEndpointRESTController.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/controller/LegacyEndpointRESTController.java
@@ -4,9 +4,7 @@
 
 package uk.gov.ons.fwmt.gateway.controller;
 
-import java.io.IOException;
-import java.util.Iterator;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +14,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import lombok.extern.slf4j.Slf4j;
-import uk.gov.ons.fwmt.gateway.entity.LegacyLeaverEntity;
 import uk.gov.ons.fwmt.gateway.entity.LegacySampleEntity;
 import uk.gov.ons.fwmt.gateway.entity.LegacyStaffEntity;
 import uk.gov.ons.fwmt.gateway.service.IngesterService;
-import uk.gov.ons.fwmt.gateway.utility.readers.LegacyLeaversReader;
 import uk.gov.ons.fwmt.gateway.utility.readers.LegacyLFSSampleReader;
 import uk.gov.ons.fwmt.gateway.utility.readers.LegacyStaffReader;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * Class for file upload controller
@@ -38,109 +36,110 @@ import uk.gov.ons.fwmt.gateway.utility.readers.LegacyStaffReader;
 @RestController
 public class LegacyEndpointRESTController {
 
-    private final IngesterService ingesterService;
+  private final IngesterService ingesterService;
 
-    @Autowired
-    public LegacyEndpointRESTController(IngesterService ingesterService) {
-        this.ingesterService = ingesterService;
+  @Autowired
+  public LegacyEndpointRESTController(IngesterService ingesterService) {
+    this.ingesterService = ingesterService;
+  }
+
+  public ResponseEntity<?> confirmFile(MultipartFile file, String endpoint) {
+    // confirm data is in correct format
+    if (!confirmFilename(file, endpoint)) {
+      return new ResponseEntity<>(
+          "The file name specified in the request does not match the file name format expected by the endpoint",
+          HttpStatus.BAD_REQUEST);
+    } else if (!confirmFiletype(file)) {
+      return new ResponseEntity<>("Recieved non-CSV file", HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    } else {
+      return null;
     }
 
-    public ResponseEntity<?> confirmFile(MultipartFile file, String endpoint) {
-        // confirm data is in correct format
-        if (!confirmFilename(file, endpoint)) {
-            return new ResponseEntity<>("The file name specified in the request does not match the file name format expected by the endpoint",HttpStatus.BAD_REQUEST);
-        } else if (!confirmFiletype(file)) {
-            return new ResponseEntity<>("Recieved non-CSV file",HttpStatus.UNSUPPORTED_MEDIA_TYPE);
-        } else {
-            return null;
-        }
+  }
 
+  public boolean confirmFilename(MultipartFile file, String endpoint) {
+    String filename = file.getOriginalFilename();
+    String[] filenameSplit = filename.split("\\.");
+    String[] nameSplit = filenameSplit[0].split("_");
+    String fileEndpoint = nameSplit[0];
+    String surveyTla = nameSplit[1];
+    String timestamp = nameSplit[2];
+    DatatypeConverter.parseDateTime(timestamp);
+    return fileEndpoint.equals(endpoint) &&
+        surveyTla.length() == 3 &&
+        !timestamp.isEmpty();
+  }
+
+  public boolean confirmFiletype(MultipartFile file) {
+    String contentType = file.getContentType();
+    String filename = file.getOriginalFilename();
+    String[] filenameSplit = filename.split("\\.");
+    return contentType.equals("text/csv") &&
+        contentType.equals("text/csv") &&
+        filenameSplit[filenameSplit.length - 1].equals("csv");
+  }
+
+  @RequestMapping(value = "/sample", method = RequestMethod.POST, produces = "application/json")
+  public ResponseEntity<?> sampleREST(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
+      throws IOException {
+
+    ResponseEntity<?> responseEntity = confirmFile(file, "sample");
+    if (!responseEntity.equals(null)) {
+      return responseEntity;
     }
 
-    public boolean confirmFilename(MultipartFile file, String endpoint) {
-        String filename = file.getOriginalFilename();
-        String[] filenameSplit = filename.split("\\.");
-        String[] nameSplit = filenameSplit[0].split("_");
-        String fileEndpoint = nameSplit[0];
-        String surveyTla = nameSplit[1];
-        String timestamp = nameSplit[2];
-        return fileEndpoint.equals(endpoint) &&
-            surveyTla.length() == 3 &&
-            !timestamp.isEmpty();
+    // add data to reception table
+    LegacyLFSSampleReader legacyLFSSampleReader = new LegacyLFSSampleReader(file.getInputStream());
+    Iterator<LegacySampleEntity> iterator = legacyLFSSampleReader.iterator();
+    ingesterService.ingestLegacySample(iterator);
+
+    if (legacyLFSSampleReader.errorList.size() != 0) {
+      // TODO handle errors
     }
 
-    public boolean confirmFiletype(MultipartFile file) {
-        String contentType = file.getContentType();
-        String filename = file.getOriginalFilename();
-        String[] filenameSplit = filename.split("\\.");
-        return contentType.equals("text/csv") &&
-            contentType.equals("text/csv") &&
-            filenameSplit[filenameSplit.length-1].equals("csv");
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @RequestMapping(value = "/staff", method = RequestMethod.POST, produces = "application/json")
+  public ResponseEntity<?> staffREST(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
+      throws IOException {
+
+    ResponseEntity<?> responseEntity = confirmFile(file, "staff");
+    if (!responseEntity.equals(null)) {
+      return responseEntity;
     }
 
-    @RequestMapping(value = "/sample", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<?> sampleREST(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
-            throws IOException {
+    // add data to reception table
+    LegacyStaffReader legacyStaffReader = new LegacyStaffReader(file.getInputStream());
+    Iterator<LegacyStaffEntity> iterator = legacyStaffReader.iterator();
+    ingesterService.ingestLegacyStaff(iterator);
 
-        ResponseEntity<?> responseEntity = confirmFile(file, "sample");
-        if (!responseEntity.equals(null)) {
-            return responseEntity;
-        }
-        
-        // add data to reception table
-        LegacyLFSSampleReader legacyLFSSampleReader = new LegacyLFSSampleReader(file.getInputStream());
-        Iterator<LegacySampleEntity> iterator = legacyLFSSampleReader.iterator();
-        ingesterService.ingestLegacySample(iterator);
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
 
-        if (legacyLFSSampleReader.errorList.size() != 0) {
-            // TODO handle errors
-        }
+  @RequestMapping(value = "/info", method = RequestMethod.GET, produces = "application/json")
+  public ResponseEntity<?> infoREST(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
+      throws IOException {
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
 
-    @RequestMapping(value = "/staff", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<?> staffREST(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
-            throws IOException {
+  // Not Currently Requried
+  //    @RequestMapping(value = "/legacy/leavers", method = RequestMethod.POST)
+  //    public ResponseEntity<?> leaversREST(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
+  //            throws IOException {
+  //
+  //        // confirm data is in correct format
+  //        if (!confirmFileType(file)) {
+  //            return new ResponseEntity<>("Invalid file format",HttpStatus.I_AM_A_TEAPOT);
+  //        }
+  //
+  //        // add data to reception table
+  //        LegacyLeaversReader legacyLeaversReader = new LegacyLeaversReader(file.getInputStream());
+  //        Iterator<LegacyLeaverEntity> iterator = legacyLeaversReader.iterator();
+  //        ingesterService.ingestLegacyLeavers(iterator);
+  //
+  //        return new ResponseEntity<>(HttpStatus.CREATED);
+  //    }
 
-        ResponseEntity<?> responseEntity = confirmFile(file, "staff");
-        if (!responseEntity.equals(null)) {
-            return responseEntity;
-        }
-        
-        // add data to reception table
-        LegacyStaffReader legacyStaffReader = new LegacyStaffReader(file.getInputStream());
-        Iterator<LegacyStaffEntity> iterator = legacyStaffReader.iterator();
-        ingesterService.ingestLegacyStaff(iterator);
-
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-    
-    @RequestMapping(value = "/info", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<?> infoREST(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
-            throws IOException {
-
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-
-    // Not Currently Requried
-//    @RequestMapping(value = "/legacy/leavers", method = RequestMethod.POST)
-//    public ResponseEntity<?> leaversREST(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
-//            throws IOException {
-//
-//        // confirm data is in correct format
-//        if (!confirmFileType(file)) {
-//            return new ResponseEntity<>("Invalid file format",HttpStatus.I_AM_A_TEAPOT);
-//        }
-//
-//        // add data to reception table
-//        LegacyLeaversReader legacyLeaversReader = new LegacyLeaversReader(file.getInputStream());
-//        Iterator<LegacyLeaverEntity> iterator = legacyLeaversReader.iterator();
-//        ingesterService.ingestLegacyLeavers(iterator);
-//
-//        return new ResponseEntity<>(HttpStatus.CREATED);
-//    }
-    
-    
 }
