16a36a696572b9b33906e032ea5fd01adf120359
ThomasPootQA <thomas.poot@qa.com>
2018-04-26 14:07:13 +0100

    Added a missing serialisation id

--- src/main/java/uk/gov/ons/fwmt/gateway/utility/readers/IllegalCSVStructureException.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/utility/readers/IllegalCSVStructureException.java
@@ -1,6 +1,9 @@
 package uk.gov.ons.fwmt.gateway.utility.readers;
 
+
 public class IllegalCSVStructureException extends Throwable {
+  private static final long serialVersionUID = 1402367242522785524L;
+
   final String[] strings;
 
   IllegalCSVStructureException() {
