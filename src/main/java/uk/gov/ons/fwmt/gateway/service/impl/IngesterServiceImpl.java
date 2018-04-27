72788622731019bef78be09e4bbcae00127f3e08
ThomasPootQA <thomas.poot@qa.com>
2018-04-27 09:22:35 +0100

    Removed the last traces of the unused leavers repo from the Ingester Service

--- src/main/java/uk/gov/ons/fwmt/gateway/service/impl/IngesterServiceImpl.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/service/impl/IngesterServiceImpl.java
@@ -2,10 +2,8 @@ package uk.gov.ons.fwmt.gateway.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.gov.ons.fwmt.gateway.entity.LegacyLeaverEntity;
 import uk.gov.ons.fwmt.gateway.entity.LegacySampleEntity;
 import uk.gov.ons.fwmt.gateway.entity.LegacyStaffEntity;
-import uk.gov.ons.fwmt.gateway.repo.reception.LegacyLeaversRepo;
 import uk.gov.ons.fwmt.gateway.repo.reception.LegacySampleRepo;
 import uk.gov.ons.fwmt.gateway.repo.reception.LegacyStaffRepo;
 import uk.gov.ons.fwmt.gateway.service.IngesterService;
@@ -18,15 +16,13 @@ public class IngesterServiceImpl implements IngesterService {
 
   private LegacySampleRepo legacySampleRepository;
   private LegacyStaffRepo legacyStaffRepository;
-  private LegacyLeaversRepo legacyLeaversRepository;
   private PublishService publishService;
 
   @Autowired
   public IngesterServiceImpl(LegacySampleRepo legacySampleRepository, LegacyStaffRepo legacyStaffRepository,
-      LegacyLeaversRepo legacyLeaversRepository, PublishService publishService) {
+    PublishService publishService) {
     this.legacySampleRepository = legacySampleRepository;
     this.legacyStaffRepository = legacyStaffRepository;
-    this.legacyLeaversRepository = legacyLeaversRepository;
     this.publishService = publishService;
   }
 
