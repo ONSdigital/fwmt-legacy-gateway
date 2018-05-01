468bb10a8c011eaf0fa261654011abdacb9066b3
ThomasPootQA <thomas.poot@qa.com>
2018-05-01 14:17:48 +0100

    Cleaned up imports

--- src/integration/java/uk/gov/ons/fwmt/gateway/integration_tests/RepoIT.java
+++ src/integration/java/uk/gov/ons/fwmt/gateway/integration_tests/RepoIT.java
@@ -1,10 +1,8 @@
 package uk.gov.ons.fwmt.gateway.integration_tests;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.ons.fwmt.gateway.Application;
