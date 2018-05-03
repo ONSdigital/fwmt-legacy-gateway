d5815e2380ec1eceef6bafab391d0c16d555f325
ThomasPootQA <thomas.poot@qa.com>
2018-05-03 09:32:31 +0100

    Altered IllegalCSVStructureException to extend Exception

--- src/main/java/uk/gov/ons/fwmt/gateway/error/IllegalCSVStructureException.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/error/IllegalCSVStructureException.java
@@ -3,7 +3,7 @@ package uk.gov.ons.fwmt.gateway.error;
 import lombok.Getter;
 import uk.gov.ons.fwmt.gateway.representation.SampleSummaryDTO;
 
-public class IllegalCSVStructureException extends Throwable {
+public class IllegalCSVStructureException extends Exception {
   private static final long serialVersionUID = 1402367242522785524L;
 
   @Getter private final String[] strings;
