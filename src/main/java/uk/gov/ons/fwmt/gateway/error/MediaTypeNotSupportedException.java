a9b161cca46176caa058d4da6347b033fcab1e00
ThomasPootQA <thomas.poot@qa.com>
2018-05-04 09:53:00 +0100

    Altered the presentation of the MediaTypeNotSupportedException

--- src/main/java/uk/gov/ons/fwmt/gateway/error/MediaTypeNotSupportedException.java
+++ src/main/java/uk/gov/ons/fwmt/gateway/error/MediaTypeNotSupportedException.java
@@ -3,7 +3,7 @@ package uk.gov.ons.fwmt.gateway.error;
 import org.springframework.http.MediaType;
 
 public class MediaTypeNotSupportedException extends Exception {
-  public MediaTypeNotSupportedException(String given, String expected) {
-    super("Expected " + expected + " but was given " + given);
+  public MediaTypeNotSupportedException(String expected, String given) {
+    super("Expected '" + expected + "' but was given '" + given + "'");
   }
 }
