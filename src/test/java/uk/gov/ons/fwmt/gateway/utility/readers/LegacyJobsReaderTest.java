6daf1b4c8abdadfdafa535f331c7a313c99ff39d
ThomasPootQA <thomas.poot@qa.com>
2018-05-08 14:47:29 +0100

    Corrected an old unit test to use updated methods

--- src/test/java/uk/gov/ons/fwmt/gateway/utility/readers/LegacyJobsReaderTest.java
+++ src/test/java/uk/gov/ons/fwmt/gateway/utility/readers/LegacyJobsReaderTest.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-
 public class LegacyJobsReaderTest {
 
   @Test
@@ -35,16 +34,16 @@ public class LegacyJobsReaderTest {
     jobType.setIdentity(jobIdentityType);
 
     //when
-    LegacyJobEntity result = LegacyJobsReader.createJobEntity(createJobRequest);
+    LegacyJobEntity result = new LegacyJobEntity(createJobRequest);
 
     //then
-    assertEquals(LegacyJobsReader.INITIAL_STATE, result.getState());
+    assertEquals(LegacyJobEntity.INITIAL_STATE, result.getState());
     assertEquals(null, result.getErroredTimeStamp());
     assertEquals(null, result.sentTimeStamp);
     assertEquals(null, result.getProcessedTimeStamp());
     assertEquals(null, result.getErroredTimeStamp());
     assertEquals(expectedJobId, result.getTmJobId());
-    assertEquals(expectedJobLocation,result.getLegacyJobId());
+    assertEquals(expectedJobLocation, result.getLegacyJobId());
   }
 
   @Test
@@ -52,17 +51,16 @@ public class LegacyJobsReaderTest {
 
     //Given
     LegacyJobEntity initialJobState = new LegacyJobEntity();
-    initialJobState.setState(LegacyJobsReader.INITIAL_STATE);
+    initialJobState.setState(LegacyJobEntity.INITIAL_STATE);
     String initialimeStamp = LocalDateTime.now().minusDays(1).toString();
     initialJobState.setSentTimeStamp(initialimeStamp);
 
     //When
-    LegacyJobEntity resultJobState = LegacyJobsReader.setStateSent(initialJobState);
-
+    initialJobState.setStateSent();
 
     //Then
-    assertFalse(resultJobState.sentTimeStamp.equals(initialimeStamp));
-    assertEquals(LegacyJobsReader.SENT_STATE,resultJobState.getState());
+    assertFalse(initialJobState.sentTimeStamp.equals(initialimeStamp));
+    assertEquals(LegacyJobEntity.SENT_STATE, initialJobState.getState());
     //    assertEquals("egg",sentJobState.getSentTimeStamp());
 
   }
@@ -72,37 +70,33 @@ public class LegacyJobsReaderTest {
 
     //Given
     LegacyJobEntity sentJobState = new LegacyJobEntity();
-    sentJobState.setState(LegacyJobsReader.SENT_STATE);
+    sentJobState.setState(LegacyJobEntity.SENT_STATE);
     String sentTimeStamp = LocalDateTime.now().minusDays(1).toString();
     sentJobState.setSentTimeStamp(sentTimeStamp);
 
     //When
-    LegacyJobEntity resultJobState = LegacyJobsReader.setStateProcessed(sentJobState);
-
+    sentJobState.setStateProcessed();
 
     //Then
-    assertFalse(resultJobState.sentTimeStamp.equals(sentTimeStamp));
-    assertEquals(LegacyJobsReader.PROCESSED_STATE,resultJobState.getState());
+    assertFalse(sentJobState.sentTimeStamp.equals(sentTimeStamp));
+    assertEquals(LegacyJobEntity.PROCESSED_STATE, sentJobState.getState());
 
   }
-
-
 
   public void whenAJobErrorsItsStateAndtimeStampShouldBeUpdated() {
 
     //Given
     LegacyJobEntity sentJobState = new LegacyJobEntity();
-    sentJobState.setState(LegacyJobsReader.SENT_STATE);
+    sentJobState.setState(LegacyJobEntity.SENT_STATE);
     String sentTimeStamp = LocalDateTime.now().minusDays(1).toString();
     sentJobState.setSentTimeStamp(sentTimeStamp);
 
     //When
-    LegacyJobEntity resultJobState = LegacyJobsReader.setStateErrored(sentJobState);
-
+    sentJobState.setStateErrored();
 
     //Then
-    assertFalse(resultJobState.sentTimeStamp.equals(sentTimeStamp));
-    assertEquals(LegacyJobsReader.ERRORED_STATE,resultJobState.getState());
+    assertFalse(sentJobState.sentTimeStamp.equals(sentTimeStamp));
+    assertEquals(LegacyJobEntity.ERRORED_STATE, sentJobState.getState());
 
   }
 }
