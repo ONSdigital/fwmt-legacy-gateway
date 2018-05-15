ce3ef810a07cc83a085de7515149801e61f947e6
ThomasPootQA <thomas.poot@qa.com>
2018-05-15 11:08:59 +0100

    Resolved the last few misconfigurations amongst settings files in order to allow integration tests to pass

--- src/main/java/uk/gov/ons/fwmt/legacy_gateway/entity/FieldPeriod.java
+++ src/main/java/uk/gov/ons/fwmt/legacy_gateway/entity/FieldPeriod.java
@@ -1,17 +1,33 @@
 package uk.gov.ons.fwmt.legacy_gateway.entity;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
 @Entity
 @Table(name = "field_periods")
 public class FieldPeriod {
-  int id;
-  Date start_date;
-  Date end_date;
-  String field_period;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Setter(AccessLevel.PRIVATE)
+  private Long id;
+
+  @Column(nullable = false)
+  Date startDate;
+
+  @Column(nullable = false)
+  Date endDate;
+
+  @Column(nullable = false)
+  String fieldPeriod;
+
+  public FieldPeriod(Date startDate, Date endDate, String fieldPeriod) {
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.fieldPeriod = fieldPeriod;
+  }
 }
